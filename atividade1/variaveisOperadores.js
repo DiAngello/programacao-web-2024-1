7b7372742b2889216fdd540b559b81a29640931b
Andressa <andressa.gabrielly.ofc@gmail.com>
2024-03-14 09:46:34 -0300

    Atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,24 @@
+//ATIVIDADE 1 - PROGRAMAÇÃO WEB
+/*1 – No repositório de atividades, crie uma pasta atividade1 e crie o arquivo variaveisOperadores.js;
+2 – Faça a declaração de 2 variáveis numéricas para cada operador e guarde o resultado da operação em uma terceira variável;
+3 – Para cada resultado obtido, imprima o texto “Resultado da {operação} entre {num_A} e {num_B}”*/
+
+let somaa = 5;
+let somab = 6;
+let soma = somaa + somab;
+console.log('Resultado da soma entre ' + somaa + ' e ' + somab +' é ' + soma);
+
+let subta = 10;
+let subtb = 2;
+let subt = subta - subtb;
+console.log('Resultado da subtração entre ' + subta + ' e ' + subtb +' é ' + subt);
+
+let multa = 4;
+let multb = 25;
+let mult = multa * multb;
+console.log('Resultado da multiplicação entre ' + multa + ' e ' + multb +' é ' + mult);
+
+let diva = 45;
+let divb = 15;
+let div = diva / divb;
+console.log('Resultado da divisão entre ' + diva + ' e ' + divb +' é ' + div);
