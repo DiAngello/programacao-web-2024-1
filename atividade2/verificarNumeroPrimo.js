19e39f86759b3bc3b3edeb7914f6d6baf4d95a18
Andressa <andressa.gabrielly.ofc@gmail.com>
2024-03-21 09:41:40 -0300

    Update Atividade 2

--- atividade2/verificarNumeroPrimo.js
+++ atividade2/verificarNumeroPrimo.js
@@ -8,15 +8,13 @@ function verificarNumeroPrimo(n){
     if (n <= 3){
         return true;
     }
-    if (n % 2 === 0 || n % 3 === 0){
+    if (n % 2 == 0 || n % 3 == 0){
         return false;
     }
-    let i = 5;
-    while (i * i <= n){
-        if(n % i === 0 ||n %(i+2)===0){
-            return false;        
+    for(let i = 3; i < n; i += 2){
+        if(n % i == 0){
+            return false;
         }
-        i +=6;
     }
     return true;
 };
