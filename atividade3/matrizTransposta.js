943822ff4539e8e6feea273d72ad616eb42d91c8
D.A <andressa.gabrielly.ofc@gmail.com>
2024-03-28 23:37:27 -0300

    Atividade 3

---
+++ atividade3/matrizTransposta.js
@@ -0,0 +1,26 @@
+/*Implemente a função transporMatriz(A);
+A função deve receber uma matriz (array de arrays) A e imprimir a matriz original e a versão transposta;*/
+
+function transporMatriz(A) {
+    console.log("Matriz Original:");
+    for (let i = 0; i < A.length; i++) {
+        console.log(A[i].join('\t'));
+    }
+
+    console.log("\nMatriz Transposta:");
+    for (let i = 0; i < A[0].length; i++) {
+        let linhaTransposta = [];
+        for (let j = 0; j < A.length; j++) {
+            linhaTransposta.push(A[j][i]);
+        }
+        console.log(linhaTransposta.join('\t'));
+    }
+}
+
+const matriz = [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+];
+
+transporMatriz(matriz);
