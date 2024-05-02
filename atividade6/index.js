11b501026f3236a09be9f2254bb2b64a0e4cefc9
Andressa <andressa.gabrielly.ofc@gmail.com>
2024-05-02 10:26:55 -0300

    Atividade 6

--- atividade6/index.js
+++ atividade6/index.js
@@ -4,3 +4,28 @@ Os dados do formulário devem ser enviados para a rota (post) "/dados" que deve 
 OBS1: na tag form, o atributo action é igual a "/dados" e o atributo method é igual a "post".
 OBS2: as páginas .html devem utilizar o BS5.*/
 
+const express =  require('express');
+const mustacheExpress = require('mustache-express');
+const app = express();
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', function(req, res){
+    res.render('index.html');
+});
+
+app.post('/dados', function(req,res){
+    let dados = req.body;
+    res.render('dados.html',{dados});
+});
+
+const PORT = 8080;
+app.listen(PORT, function(){
+    console.log("App rodando na porta " + PORT);
+});
+
+
