78b3c84a225872f4715889cbf1e100deec1fc84e
Andressa <andressa.gabrielly.ofc@gmail.com>
2024-05-29 18:48:53 -0300

    Atividade Calculadora

--- app_calc/src/routes/calculadoraRoutes.js
+++ app_calc/src/routes/calculadoraRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const router = express.Router();
-const calcController = require('../controller/calculadoraController')
-
+const router  = express.Router();
+const calcController = require('../controller/calculadoraController');
 
 router.get('/', calcController.index);
 router.post('/resultado', calcController.resultado);
 
 module.exports = router;
-
